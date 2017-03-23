25c47c3e5b1b31feb31b3a963e2229d3e8fcef44
versusx <armannet123@mail.ru>
2017-03-23 15:31:26 +0500

    f

---
+++ YS7pro-vendor.mk
@@ -0,0 +1,10 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/yunsong/YS7pro/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/yunsong/YS7pro/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/yunsong/YS7pro/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/yunsong/YS7pro/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/yunsong/YS7pro/xbin,system/xbin)
+
